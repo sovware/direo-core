2b7e10ad2631f11b3169c39b4bebc3007cbe123c
themeaazz <43126377+themeaazz@users.noreply.github.com>
2021-02-21 10:13:30 +0600

    improved

--- elementor/direo-elementor.php
+++ elementor/direo-elementor.php
@@ -36,47 +36,48 @@ final class DireoWidgets {
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Accordion() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_AddListing_Form() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Blogs() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Categories() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Checkout() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Logos() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Profile() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_ContactForm() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_ContactItems() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Counter() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Dashboard() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_FeatureBox() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Listings() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_ListingsCarousel() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_ListingsMap() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Locations() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Login() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Registration() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Booking_Confirmation() );
-
-		if ( class_exists( 'Post_Your_Need' ) ) {
-			Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedCategories() );
-			Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedLocations() );
-			Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedSingleCat() );
-			Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedSingleLoc() );
-			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Needs() );
-		}
-
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Payment() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_PricingPlan() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchForm() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchResult() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchResultMap() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleCat() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleCatMap() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleLoc() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleLocMap() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleTag() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleTagMap() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Testimonial() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Heading() );
 		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Subscribe() );
-		Plugin::instance()->widgets_manager->register_widget_type( new Direo_Transaction() );
 		Plugin::instance()->widgets_manager->register_widget_type( new CTA() );
+
+		if ( class_exists( 'Directorist_Base' ) ) {
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Categories() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Checkout() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Profile() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_ContactItems() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Dashboard() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Listings() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_ListingsCarousel() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_ListingsMap() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Locations() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Login() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Registration() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Booking_Confirmation() );
+			if ( class_exists( 'Post_Your_Need' ) ) {
+				Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedCategories() );
+				Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedLocations() );
+				Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedSingleCat() );
+				Plugin::instance()->widgets_manager->register_widget_type( new Direo_NeedSingleLoc() );
+				Plugin::instance()->widgets_manager->register_widget_type( new Direo_Needs() );
+			}
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Payment() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_PricingPlan() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchForm() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchResult() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SearchResultMap() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleCat() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleCatMap() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleLoc() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleLocMap() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleTag() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_SingleTagMap() );
+			Plugin::instance()->widgets_manager->register_widget_type( new Direo_Transaction() );
+		}
 	}
 
 	public function direo_widget_category( $manager ) {
